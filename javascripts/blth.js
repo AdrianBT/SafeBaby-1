96c8f6fea38a7f8c2322d73608b61b2df2331079
EvyEve <cedent.eva@gmail.com>
2018-11-14 15:54:44 -0500

    Added Bluetooth javascript file

---
+++ javascripts/blth.js
@@ -0,0 +1,46 @@
+// navigator.bluetooth.requestDevice({
+//   acceptAllDevices: true,
+//   optionalServices: ['battery_service']
+// })
+// .then(device => { /* ... */ })
+// .catch(error => { console.log(error); });
+function onButtonClick() {
+  let filters = [];
+
+  let filterService = document.querySelector('#service').value;
+  if (filterService.startsWith('0x')) {
+    filterService = parseInt(filterService);
+  }
+  if (filterService) {
+    filters.push({services: [filterService]});
+  }
+
+  let filterName = document.querySelector('#name').value;
+  if (filterName) {
+    filters.push({name: filterName});
+  }
+
+  let filterNamePrefix = document.querySelector('#namePrefix').value;
+  if (filterNamePrefix) {
+    filters.push({namePrefix: filterNamePrefix});
+  }
+
+  let options = {};
+  if (document.querySelector('#allDevices').checked) {
+    options.acceptAllDevices = true;
+  } else {
+    options.filters = filters;
+  }
+
+  log('Requesting Bluetooth Device...');
+  log('with ' + JSON.stringify(options));
+  navigator.bluetooth.requestDevice(options)
+  .then(device => {
+    document.getElementById("devName").innerHTML = device.name;
+    document.getElementById("idNum").innerHTML = device.id;
+    document.getElementById("conct").innerHTML = device.gatt.connected;
+  })
+  .catch(error => {
+    log('Argh! ' + error);
+  });
+}
